eaee9e3d5a9c082838df91e703456871778fd90b
Powerlated <zjia08@gmail.com>
2020-09-28 20:00:44 -0400

    Implement SRAM

--- src/emulator/saving/Sram.cs
+++ src/emulator/saving/Sram.cs
@@ -4,24 +4,34 @@ namespace OptimeGBA
 {
     public class Sram : SaveProvider
     {
+        byte[] Memory = new byte[65536];
+
         public override byte Read8(uint addr)
         {
+            addr -= 0xE000000;
+            if (addr < Memory.Length) {
+                return Memory[addr];
+            }
             return 0;
         }
 
         public override void Write8(uint addr, byte val)
         {
-
+            addr -= 0xE000000;
+            if (addr < Memory.Length) {
+                Memory[addr] = val;
+                Dirty = true;
+            }
         }
 
         public override byte[] GetSave()
         {
-            return new byte[0];
+            return Memory;
         }
 
         public override void LoadSave(byte[] save)
         {
-
+            save.CopyTo(Memory, 0);
         }
     }
 }
