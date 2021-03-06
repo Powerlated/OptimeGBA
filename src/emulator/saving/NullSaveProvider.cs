d2efe346d5975dd3aa54261598d3a5904aba9ec1
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2021-03-06 16:37:23 -0500

    Note on potential improvement

--- src/emulator/saving/NullSaveProvider.cs
+++ src/emulator/saving/NullSaveProvider.cs
@@ -6,22 +6,22 @@ namespace OptimeGBA
     {
         public override byte Read8(uint addr)
         {
-            return 0;
+            return 0; // Consider throwing NotImplementedException?
         }
 
         public override void Write8(uint addr, byte val)
         {
-
+            // Consider throwing NotImplementedException?
         }
 
         public override byte[] GetSave()
         {
-            return new byte[0];
+            return new byte[0]; // Consider throwing NotImplementedException?
         }
 
         public override void LoadSave(byte[] save)
         {
-
+            // Consider throwing NotImplementedException?
         }
     }
 }
